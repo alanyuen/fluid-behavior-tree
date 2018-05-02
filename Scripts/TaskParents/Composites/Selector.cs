c76625bdcca4940666936e08ebd42ecf90210b1b
Charles Brindle <ash@blueashes.com>
2018-05-02 15:10:25 -0600

    Added test case for conditional abort

---
+++ Scripts/TaskParents/Composites/Selector.cs
@@ -0,0 +1,60 @@
+﻿using Adnc.FluidBT.Tasks;
+
+namespace Adnc.FluidBT.TaskParents {
+    public class Selector : CompositeBase {
+        protected override TaskStatus OnUpdate () {
+            if (AbortSelf(TaskStatus.Success)) {
+                return Update();
+            }
+            
+            foreach (var abort in AbortLowerPriorities) {
+                if (abort.GetAbortStatus() != TaskStatus.Success) continue;
+                children[ChildIndex].End();
+                Reset();
+                // @TODO Should use the abort's index to figure out what to revaluate (should not run entire branch)
+                return Update();
+            }
+            
+            for (var i = ChildIndex; i < children.Count; i++) {
+                var child = children[ChildIndex];
+
+                switch (child.Update()) {
+                    case TaskStatus.Success:
+                        return TaskStatus.Success;
+                    case TaskStatus.Continue:
+                        return TaskStatus.Continue;
+                }
+                
+                if (child.IsLowerPriority) {
+                    var abortCondition = child.GetAbortCondition();
+                    if (abortCondition != null) {
+                        // @TODO Abort must include the current child index
+                        AbortLowerPriorities.Add(abortCondition);
+                    }
+                }
+
+                ChildIndex++;
+            }
+
+            return TaskStatus.Failure;
+        }
+
+        public override ITask GetAbortCondition () {
+            return this;
+        }
+
+        // @TODO Cache the abort children on first run since they will never change
+        public override TaskStatus GetAbortStatus () {
+            foreach (var child in children) {
+                var abort = child.GetAbortCondition();
+                if (abort == null) continue;
+
+                if (abort.GetAbortStatus() == TaskStatus.Success) {
+                    return TaskStatus.Success;
+                }
+            }
+
+            return TaskStatus.Failure;
+        }
+    }
+}
---
+++ Scripts/Tasks/ITask.cs
@@ -0,0 +1,45 @@
+﻿namespace Adnc.FluidBT.Tasks {
+    public interface ITask {
+        /// <summary>
+        /// Is this task enabled or not? Disabled tasks are excluded from the runtime
+        /// </summary>
+        bool Enabled { get; set; }
+        
+        /// <summary>
+        /// Check if this node is a valid lower priority abort
+        /// </summary>
+        bool IsLowerPriority { get; }
+        
+        /// <summary>
+        /// Triggered every tick
+        /// </summary>
+        /// <returns></returns>
+        TaskStatus Update ();
+
+        /// <summary>
+        /// Returns the valid abort condition
+        /// </summary>
+        /// <returns></returns>
+        ITask GetAbortCondition ();
+
+        /// <summary>
+        /// Forcibly end this task. Firing all necessary completion logic
+        /// </summary>
+        void End ();
+
+        /// <summary>
+        /// Reset this task back to its initial state. Hard reset should run all
+        /// one time run only logic (meant for pooling)
+        /// </summary>
+        /// <param name="hardReset"></param>
+        void Reset (bool hardReset = false);
+        
+        /// <summary>
+        /// Query this parent task to get the correct corresponding abort type
+        /// Example selector tries all valid conditions until success (or returns failure), while
+        /// sequence returns first item 
+        /// </summary>
+        /// <returns></returns>
+        TaskStatus GetAbortStatus ();
+    }
+}
